bb47efd636f289a8a7062de101ddce86b9368985
PScoriae <pierreccesario@gmail.com>
2021-08-26 22:06:37 +0800

    fixed error in docker shell script

--- docker.sh
+++ docker.sh
@@ -3,5 +3,5 @@
 # automates docker image creation and push to local registry.
 
 docker build -t imyourjoy .
-docker tag push imyourjoy 192.168.0.154:5000/imyourjoy
+docker tag imyourjoy 192.168.0.154:5000/imyourjoy
 docker push 192.168.0.154:5000/imyourjoy
